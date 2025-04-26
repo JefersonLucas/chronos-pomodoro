ca830ed68705c9c047e5f136194e0ca7d9ee0d75
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-26 17:17:53 -0300

    feat: added title attribute

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,19 +4,19 @@ import styles from "./styles.module.css"
 export function Menu() {
 	return (
 		<nav className={styles.menu}>
-			<a className={styles.menuLink} href="#">
+			<a className={styles.menuLink} title="Início" href="#">
 				<HouseIcon />
 			</a>
 
-			<a className={styles.menuLink} href="#">
+			<a className={styles.menuLink} title="Histórico" href="#">
 				<HistoryIcon />
 			</a>
 
-			<a className={styles.menuLink} href="#">
+			<a className={styles.menuLink} title="Configurações" href="#">
 				<SettingsIcon />
 			</a>
 
-			<a className={styles.menuLink} href="#">
+			<a className={styles.menuLink} title="tema" href="#">
 				<SunIcon />
 			</a>
 		</nav>
