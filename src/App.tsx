06d7dd78e881690a287fd76d3cabf52baacd49d4
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-27 09:16:23 -0300

    feat: used Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,10 @@
 import "./styles/global.css"
 import "./styles/theme.css"
 
-import { PlayCircleIcon } from "lucide-react"
-
-import { Container } from "./components/Container"
-import { CountDown } from "./components/CountDown"
-import { Cycles } from "./components/Cycles"
-import { DefaultButton } from "./components/DefaultButton"
-import { DefaultInput } from "./components/DefaultInput"
-import { Footer } from "./components/Footer"
-import { Logo } from "./components/Logo"
-import { Menu } from "./components/Menu"
+import { HomePage } from "./pages/Home"
+// import { NotFoundPage } from "./pages/NotFound"
 
 export function App() {
-	return (
-		<>
-			<Container>
-				<Logo />
-			</Container>
-
-			<Container>
-				<Menu />
-			</Container>
-
-			<Container>
-				<CountDown />
-			</Container>
-
-			<Container>
-				<form className="form" action="">
-					<div className="formRow">
-						<DefaultInput
-							labelText="Task"
-							id="meuInput"
-							type="text"
-							title="Descrição"
-							placeholder="Digite algo"
-						/>
-					</div>
-
-					<div className="formRow">
-						<p>Lorem ipsum dolor sit amet.</p>
-					</div>
-
-					<div className="formRow">
-						<Cycles />
-					</div>
-
-					<div className="formRow">
-						<DefaultButton
-							icon={<PlayCircleIcon />}
-							color="green"
-							title="Iniciar"
-						/>
-					</div>
-				</form>
-			</Container>
-
-			<Container>
-				<Footer />
-			</Container>
-		</>
-	)
+	return <HomePage />
+	// return <NotFoundPage />
 }
