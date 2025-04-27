3f8edce0b10900f341b69b1a3b4547a4faa1bc69
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-27 09:15:24 -0300

    feat: created NotFound page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,19 @@
+import { Container } from "../../components/Container"
+import { MainTemplate } from "../../templates/Main"
+
+export function NotFoundPage() {
+	return (
+		<MainTemplate>
+			<Container>
+				<h1>Página não encontrada</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Sit ratione atque voluptas voluptate eius nesciunt
+					voluptates quaerat nobis beatae sequi. Accusantium quis,
+					soluta omnis quam laboriosam accusamus corrupti voluptatem
+					molestias.
+				</p>
+			</Container>
+		</MainTemplate>
+	)
+}
