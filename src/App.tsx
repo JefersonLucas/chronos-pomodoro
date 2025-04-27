00e332e2a4143458a184ec8a35783eddce7776b8
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-27 10:03:47 -0300

    feat: used Home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import "./styles/global.css"
 import "./styles/theme.css"
 
 import { HomePage } from "./pages/Home"
-// import { NotFoundPage } from "./pages/NotFound"
 
 export function App() {
 	return <HomePage />
-	// return <NotFoundPage />
 }
