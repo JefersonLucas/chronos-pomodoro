b0c2f7a5ea10d0429feda6fd39fc7ef46e45a2dd
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-28 13:37:58 -0300

    feat: displayed list of cycles

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,43 +1,37 @@
+import { useTaskContext } from "../../hooks/useTaskContext"
+import { getNextCycle } from "../../utils/getNextCycle"
+import { getNextCycleType } from "../../utils/getNextCycleType"
 import styles from "./styles.module.css"
 
 export function Cycles() {
+	const { state } = useTaskContext()
+
+	const cycleStep = Array.from({ length: state.currentCycle })
+
+	const cycleDescription = {
+		workTime: "foco",
+		shortBreakTime: "decanso curto",
+		longBreakTime: "descanso longo",
+	}
+
 	return (
 		<div className={styles.cycles}>
 			<span>Ciclos:</span>
 
 			<div className={styles.cycleDots}>
-				<span
-					title="Trabalho"
-					className={`${styles.cycleDot} ${styles.workTime}`}
-				></span>
-				<span
-					title="Descanso curto"
-					className={`${styles.cycleDot} ${styles.shortBreakTime}`}
-				></span>
-				<span
-					title="Trabalho"
-					className={`${styles.cycleDot} ${styles.workTime}`}
-				></span>
-				<span
-					title="Descanso curto"
-					className={`${styles.cycleDot} ${styles.shortBreakTime}`}
-				></span>
-				<span
-					title="Trabalho"
-					className={`${styles.cycleDot} ${styles.workTime}`}
-				></span>
-				<span
-					title="Descanso curto"
-					className={`${styles.cycleDot} ${styles.shortBreakTime}`}
-				></span>
-				<span
-					title="Trabalho"
-					className={`${styles.cycleDot} ${styles.workTime}`}
-				></span>
-				<span
-					title="Descanso longo"
-					className={`${styles.cycleDot} ${styles.longBreakTime}`}
-				></span>
+				{cycleStep.map((_, index) => {
+					const nextCycle = getNextCycle(index)
+					const nextCycleType = getNextCycleType(nextCycle)
+
+					return (
+						<span
+							key={`${nextCycle}_${index}`}
+							className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+							aria-label={`Indicador de ciclo de ${cycleDescription[nextCycleType]}`}
+							title={`Indicador de ciclo de ${cycleDescription[nextCycleType]}`}
+						></span>
+					)
+				})}
 			</div>
 		</div>
 	)
