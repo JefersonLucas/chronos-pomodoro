27ea35087a9f1af4dc3cea1b440cbffa186f9a31
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-28 11:48:34 -0300

    feat: added doc on type models

--- src/models/TaskModel.d.ts
+++ src/models/TaskModel.d.ts
@@ -1,5 +1,15 @@
 import { TaskStateModel } from "./TaskStateModel"
 
+/**
+ * Representa a tarefa.
+ * @property {string} id O identificador da tarefa.
+ * @property {string} name O nome da tarefa.
+ * @property {number} duration A duração da tarefa.
+ * @property {number} startDate O momento que iniciou a tarefa.
+ * @property {number} completeDate O momento que a tarefa finalizou.
+ * @property {number} interruptDate O momento que a tarefa foi interrompida.
+ * @property {TaskStateModel["cycle"]} type O tipo de ciclo da tarefa.
+ */
 export type TaskModel = {
 	id: string
 	name: string
--- src/models/TaskStateModel.d.ts
+++ src/models/TaskStateModel.d.ts
@@ -1,14 +1,33 @@
 import { TaskModel } from "./TaskModel"
 
+/**
+ * Representa o estado atual de uma tarefa.
+ *
+ * @property {TaskModel[]} tasks As tarefas.
+ * @property {number} secondsRemaining Os segundos restantes para a tarefa ser concluída.
+ * @property {number} formattedSecondsRemaining Os segundos restantes formatado no padrão `"mm:ss"`.
+ * @property {TaskModel | null} activeTask Tarefa ativa no ciclo, podendo possuir uma tarefa ou não.
+ * @property {number} currentCycle O ciclo atual de uma tarefa. Esse propriedade tem o valor que vai de 0 até 8.
+ * @property {CycleModel} config Os ciclos e o valor em minutos de cada um.
+ */
 export type TaskStateModel = {
 	tasks: TaskModel[]
 	secondsRemaining: number
 	formattedSecondsRemaining: string
 	activeTask: TaskModel | null
-	currentCycle: number // 1 a 8
-	config: {
-		workTime: number
-		shortBreakTime: number
-		longBreakTime: number
-	}
+	currentCycle: number
+	config: CycleModel
+}
+
+/**
+ * Representa cada tipo de ciclo de uma tarefa e o valor em minutos de cada um.
+ *
+ * @property {number} workTime Valor em minutos em um momento de foco.
+ * @property {number} shortBreakTime Valor em minutos em um momento descanso curto.
+ * @property {number} longBreakTime Valor em minutos em um momento de descanso longo.
+ */
+type CycleModel = {
+	workTime: number
+	shortBreakTime: number
+	longBreakTime: number
 }
