3a9dd3f7f1138b217faa069e355951eb03789efb
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-30 10:48:49 -0300

    feat: used worker for count down seconds

--- src/providers/Task/index.tsx
+++ src/providers/Task/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useReducer } from "react"
-import { initialTaskState } from "../../contexts/Task/initialTaskState"
 import { TaskContext } from "../../contexts/Task"
+import { initialTaskState } from "../../contexts/Task/initialTaskState"
+import { TaskActionTypes } from "../../models/TaskActionModel"
 import { taskReducer } from "../../reducers/taskReducer"
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 
 type TaskContextProviderProps = {
 	children: React.ReactNode
@@ -10,9 +12,36 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	const [state, dispatch] = useReducer(taskReducer, initialTaskState)
 
+	const worker = TimerWorkerManager.getInstance()
+
+	worker.onmessage((event) => {
+		// Contador decrescente do Worker
+		const countDownSeconds = event.data
+
+		// Valida se o contador finalizou
+		if (countDownSeconds <= 0) {
+			dispatch({
+				type: TaskActionTypes.COMPLETE_TASK,
+			})
+			worker.terminate()
+		} else {
+			dispatch({
+				type: TaskActionTypes.COUNT_DOWN,
+				payload: { secondsRemaining: countDownSeconds },
+			})
+		}
+	})
+
 	useEffect(() => {
 		console.log(state)
-	}, [state])
+
+		if (!state.activeTask) {
+			console.log("Worker terminado por falta de activeTask")
+			worker.terminate()
+		}
+
+		worker.postMessage(state)
+	}, [worker, state])
 
 	return (
 		<TaskContext.Provider value={{ state, dispatch }}>
