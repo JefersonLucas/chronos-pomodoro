315a14cedd3a27178cde11cc52dd8a330ae6dd96
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-04-30 10:46:46 -0300

    feat: added COUNT_DOWN and COMPLETE_TASK action types

--- src/models/TaskActionModel.ts
+++ src/models/TaskActionModel.ts
@@ -1,15 +1,23 @@
 import { TaskModel } from "./TaskModel"
+import { TaskStateModel } from "./TaskStateModel"
 
 export enum TaskActionTypes {
 	START_TASK = "START_TASK",
-	INTERRUPT_TASK = "INTERRUPT_TASK",
+	COUNT_DOWN = "COUNT_DOWN",
 	RESET_STATE = "RESET_STATE",
+	INTERRUPT_TASK = "INTERRUPT_TASK",
+	COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionsWithPayload = {
-	type: TaskActionTypes.START_TASK
-	payload: TaskModel
-}
+export type TaskActionsWithPayload =
+	| {
+			type: TaskActionTypes.START_TASK
+			payload: TaskModel
+	  }
+	| {
+			type: TaskActionTypes.COUNT_DOWN
+			payload: Pick<TaskStateModel, "secondsRemaining">
+	  }
 
 export type TaskActionsWithoutPayload =
 	| {
@@ -17,6 +25,9 @@ export type TaskActionsWithoutPayload =
 	  }
 	| {
 			type: TaskActionTypes.INTERRUPT_TASK
+	  }
+	| {
+			type: TaskActionTypes.COMPLETE_TASK
 	  }
 
 export type TaskActionModel = TaskActionsWithPayload | TaskActionsWithoutPayload
