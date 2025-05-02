1ca8b5ecdaad570aaaa4c7c6d91fb6d3a91e3120
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-05-02 07:10:24 -0300

    feat: created MainRouter router

---
+++ src/routers/MainRouter/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react"
+import { BrowserRouter, Route, Routes, useLocation } from "react-router"
+import { AboutPage } from "../../pages/About"
+import { HomePage } from "../../pages/Home"
+import { NotFoundPage } from "../../pages/NotFound"
+
+/**
+ * Faz o scroll para o topo de modo suave.
+ * @returns {null} não possui retorno
+ */
+function ScrollToTop(): null {
+	const { pathname } = useLocation()
+
+	useEffect(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" })
+	}, [pathname])
+
+	return null
+}
+
+export function MainRouter() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about/" element={<AboutPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+			<ScrollToTop />
+		</BrowserRouter>
+	)
+}
