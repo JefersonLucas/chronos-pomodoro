fd8edc7ae82e6a2b968693d5b6f43a422d6c9259
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-05-03 08:31:33 -0300

    docs: added documentation

--- src/contexts/Task/initialTaskState.ts
+++ src/contexts/Task/initialTaskState.ts
@@ -1,5 +1,19 @@
 import { TaskStateModel } from "../../models/TaskStateModel"
 
+/**
+ * Estado inicial das tarefas.
+ *
+ * Aqui temos as propriedades:
+ * - `tasks`: array de `tasks` vazio;
+ * - `secondsRemaining`: segundos restantes definido como `0`;
+ * - `formattedSecondsRemaining`: segundos restantes formatado definido como `"00:00"`;
+ * - `activeTask`: tarefa ativa como `null`;
+ * - `currentCycle`: ciclo atual como `0`;
+ * - `cycle`: objeto do ciclo com valores padrões:
+ * 	- `workTime`: ciclo de foco definido para `25` (minutos);
+ * 	- `shortBreakTime`: ciclo de descanso curto definido para `5` (minutos);
+ * 	- `longBreakTime`: ciclo de descanso longo definido para `15` (minutos).
+ */
 export const initialTaskState: TaskStateModel = {
 	tasks: [],
 	secondsRemaining: 0,
