9a8de4cfae534b62e3b5835cccb2bb931dcc5214
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-05-03 09:33:55 -0300

    feat: added confirm adapter

--- src/adapters/showMessage.ts
+++ src/adapters/showMessage.ts
@@ -1,4 +1,5 @@
 import { toast } from "react-toastify"
+import { Dialog } from "../components/Dialog"
 
 /**
  * Um [adapter](https://refactoring.guru/design-patterns/adapter) para o [React-toastify](https://fkhadra.github.io/react-toastify/introduction).
@@ -9,6 +10,7 @@ import { toast } from "react-toastify"
  * @property {} warning Mensagem de alerta.
  * @property {} info Mensagem de informação.
  * @property {} dismiss Limpa as mensagens.
+ * @property {} confirm Mensagem de confirmação.
  */
 export const showMessage = {
 	success: (msg: string) => toast.success(msg),
@@ -17,4 +19,17 @@ export const showMessage = {
 	warning: (msg: string) => toast.warning(msg),
 	info: (msg: string) => toast.info(msg),
 	dismiss: () => toast.dismiss(),
+	confirm: (data: string, onClosing: (confirmation: boolean) => void) =>
+		toast(Dialog, {
+			data,
+			onClose: (confirmation) => {
+				if (confirmation) return onClosing(true)
+				return onClosing(false)
+			},
+			position: "top-center",
+			autoClose: false,
+			closeOnClick: false,
+			closeButton: false,
+			draggable: false,
+		}),
 }
