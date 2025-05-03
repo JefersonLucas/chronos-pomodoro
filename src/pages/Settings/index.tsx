86e588c3862bbb6e0de3579ac043f1ea3d89321c
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-05-03 10:50:48 -0300

    feat: added validate on save settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,11 +1,54 @@
 import { SaveIcon } from "lucide-react"
+import { useRef } from "react"
+import { showMessage } from "../../adapters/showMessage"
 import { Container } from "../../components/Container"
 import { DefaultButton } from "../../components/DefaultButton"
 import { DefaultInput } from "../../components/DefaultInput"
 import { Heading } from "../../components/Heading"
+import { useTaskContext } from "../../hooks/useTaskContext"
 import { MainTemplate } from "../../templates/Main"
 
 export function SettingsPage() {
+	const { state } = useTaskContext()
+	const workTimeInput = useRef<HTMLInputElement>(null)
+	const shortBreakTimeInput = useRef<HTMLInputElement>(null)
+	const longBreakTimeInput = useRef<HTMLInputElement>(null)
+
+	function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+		showMessage.dismiss()
+
+		const formErrors = []
+
+		const workTime = Number(workTimeInput.current?.value)
+		const shortBreakTime = Number(shortBreakTimeInput.current?.value)
+		const longBreakTime = Number(longBreakTimeInput.current?.value)
+
+		if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+			formErrors.push("Digite apenas números para TODOS os campos")
+		}
+
+		if (workTime < 1 || workTime > 99) {
+			formErrors.push("Digite valores entre 1 e 99 para foco")
+		}
+
+		if (shortBreakTime < 1 || shortBreakTime > 30) {
+			formErrors.push("Digite valores entre 1 e 30 para descanso curto")
+		}
+
+		if (longBreakTime < 1 || longBreakTime > 60) {
+			formErrors.push("Digite valores entre 1 e 60 para descanso longo")
+		}
+
+		if (formErrors.length > 0) {
+			formErrors.forEach((error) => {
+				showMessage.error(error)
+			})
+			return
+		}
+
+		console.log("SALVAR")
+	}
 	return (
 		<MainTemplate>
 			<Container>
@@ -20,20 +63,32 @@ export function SettingsPage() {
 			</Container>
 
 			<Container>
-				<form action="" className="form">
+				<form onSubmit={handleSaveSettings} className="form">
 					<div className="formRow">
-						<DefaultInput id="workTime" labelText="Foco" />
-					</div>
-					<div className="formRow">
+						<DefaultInput
+							id="workTime"
+							labelText="Foco (minutos)"
+							type="number"
+							ref={workTimeInput}
+							defaultValue={state.cycle.workTime}
+						/>
+						<div className="formRow"></div>
+
 						<DefaultInput
 							id="shortBreakTime"
-							labelText="Descanso curto"
+							labelText="Descanso curto (minutos)"
+							ref={shortBreakTimeInput}
+							type="number"
+							defaultValue={state.cycle.shortBreakTime}
 						/>
-					</div>
-					<div className="formRow">
+						<div className="formRow"></div>
+
 						<DefaultInput
 							id="longBreakTime"
-							labelText="Descanso longo"
+							labelText="Descanso longo (minutos)"
+							type="number"
+							ref={longBreakTimeInput}
+							defaultValue={state.cycle.longBreakTime}
 						/>
 					</div>
 					<div className="formRow">
@@ -41,6 +96,7 @@ export function SettingsPage() {
 							icon={<SaveIcon />}
 							aria-label="Salvar configurações"
 							title="Salvar configurações"
+							type="submit"
 						/>
 					</div>
 				</form>
