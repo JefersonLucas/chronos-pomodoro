ef62d7d4532ab26e53154d5526d3b80271b000a2
Jeferson Lucas <jeferson.luckas@gmail.com>
2025-05-05 19:26:30 -0300

    feat: added lazy initialization of theme

--- src/providers/Theme/index.tsx
+++ src/providers/Theme/index.tsx
@@ -1,6 +1,7 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { ThemeContext } from "../../contexts/Theme"
 import { initialThemeState } from "../../contexts/Theme/initialThemeState"
+import { ThemeModel } from "../../models/ThemeModel"
 import { themeReducer } from "../../reducers/themeReducer"
 
 type ThemeContextProviderProps = {
@@ -8,7 +9,23 @@ type ThemeContextProviderProps = {
 }
 
 export function ThemeContextProvider({ children }: ThemeContextProviderProps) {
-	const [state, dispatch] = useReducer(themeReducer, initialThemeState)
+	const [state, dispatch] = useReducer(
+		themeReducer,
+		initialThemeState,
+		() => {
+			// Recuperando do localStorage
+			const storageTheme =
+				(localStorage.getItem("theme") as ThemeModel) || "dark"
+			return storageTheme
+		},
+	)
+
+	useEffect(() => {
+		// Modificando o data attibute data-theme
+		document.documentElement.setAttribute("data-theme", state)
+		// Salvando no localStorage
+		localStorage.setItem("theme", state)
+	}, [state])
 
 	return (
 		<ThemeContext.Provider value={{ state, dispatch }}>
